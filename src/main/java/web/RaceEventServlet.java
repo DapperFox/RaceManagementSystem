d881d41de3438164abc89f2827bcd7ab11982c21
Jared Perkins <jperkins@student.neumont.edu>
2014-05-20 14:38:10 -0600

    Added functionality to the servlet

--- src/main/java/web/RaceEventServlet.java
+++ src/main/java/web/RaceEventServlet.java
@@ -1,6 +1,9 @@
 package web;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,13 +13,35 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/*")
 public class RaceEventServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	String regexCreatePattern = "/race/create";
+	String regexRacePattern = "(/race/)([0-9]+)(/[A-Za-z]*)?";
+	
+	
     public RaceEventServlet() {
 
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		String uriString = request.getRequestURI();
+		
+		Pattern pattern = Pattern.compile(regexCreatePattern);
+		Matcher matcher = pattern.matcher(uriString);
+		
+		if(matcher.matches()){
+			//Create a new race
+			
+		}
+		
+		pattern = Pattern.compile(regexRacePattern);
+		matcher = pattern.matcher(uriString);
+		
+		if(matcher.find()){
+			
+		}
+		
+		
+		
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
