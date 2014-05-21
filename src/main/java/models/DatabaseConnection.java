07d72722a6f4a93030c49254215bee2bc57a3f1e
DapperFox <blakesterbishop@gmail.com>
2014-05-21 15:55:28 -0600

    Worked on DB Connection with Postgres

---
+++ src/main/java/models/DatabaseConnection.java
@@ -0,0 +1,37 @@
+package models;
+
+import org.apache.commons.dbcp.BasicDataSource;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+
+/**
+ * Created by blakebishop on 5/21/14.
+ */
+public class DatabaseConnection extends BasicDataSource {
+    private String username, password, jdbcURL, jdbcDriver;
+
+    public DatabaseConnection() {
+        initConnection();
+    }
+    public void initConnection() {
+        //todo fix this so that it isn't dumb
+        URI dbUri = null;
+        try {
+            dbUri = new URI(System.getenv("DATABASE_URL"));
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        this.username = dbUri.getUserInfo().split(":")[0];
+        this.password = dbUri.getUserInfo().split(":")[1];
+        this.jdbcURL = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        this.jdbcDriver = "org.postgresql.Driver";
+    }
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return Logger.getLogger("Dummy Logger");
+    }
+}
