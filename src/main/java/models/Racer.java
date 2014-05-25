e78aab4ee6a060749c64186a3ae28dc5348c84ae
Jared Perkins <jperkins@student.neumont.edu>
2014-05-24 20:52:02 -0600

    Created adress model and racer model. And beans.xml is lame.

---
+++ src/main/java/models/Racer.java
@@ -0,0 +1,37 @@
+package models;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+public class Racer {
+
+	@Id
+	@Column(name="id")
+	@SequenceGenerator(name="RaceEventSequence", sequenceName="raceEvent_seq", initialValue=1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="RaceEventSequence")
+	private Long id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="gender")
+	private String gender;
+	
+	@Column(name="birth_date")
+	private String birthDate;
+	
+	@Column(name="address")
+	private Address address;
+	
+	@Column(name="phone_number")
+	private String phoneNumber;
+}
