9e3d37b08d49dfa668f931cc517fcb52154ca81e
Jared Perkins <jperkins@student.neumont.edu>
2014-05-25 18:12:40 -0600

    Added some database syntax for the racer object

--- src/main/java/models/Racer.java
+++ src/main/java/models/Racer.java
@@ -1,10 +1,15 @@
 package models;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="racer")
 
 public class Racer {
 
