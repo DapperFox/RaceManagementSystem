3f0f64c8d6579ffafa723152d36088cad8edadf6
Jared Perkins <jperkins@student.neumont.edu>
2014-05-28 14:04:40 -0600

    Added database syntax to the address object. Deleted the /raceDetails url thing.

--- src/main/java/models/Address.java
+++ src/main/java/models/Address.java
@@ -1,14 +1,26 @@
 package models;
 
-public class Address {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
-	private String address1;
-	private String address2;
-	private String city;
-	private String state;
-	private int zipCode;
+@Entity
+@Table(name="address")
+
+public class Address {
 	
-	public Address(String address1, String address2, String city, String state, int zipCode){
+	@Column(name="address_one")
+	private String address1;
+	@Column(name="address_two")
+	private String address2;
+	@Column(name="city")
+	private String city;
+	@Column(name="state")
+	private String state;
+	@Column(name="zipCode")
+	private String zipCode;
+	
+	public Address(String address1, String address2, String city, String state, String zipCode){
 		this.address1 = address1;
 		this.address2 = address2;
 		this.city = city;
@@ -20,20 +32,20 @@ public class Address {
 	public String getAddress1() {
 		return address1;
 	}
-
+	
 	public String getAddress2() {
 		return address2;
 	}
-
+	
 	public String getCity() {
 		return city;
 	}
-
+	
 	public String getState() {
 		return state;
 	}
-
-	public int getZipCode() {
+	
+	public String getZipCode() {
 		return zipCode;
 	}
 	
--- src/main/java/web/RaceEventServlet.java
+++ src/main/java/web/RaceEventServlet.java
@@ -16,90 +16,96 @@ import models.ModelAndView;
 import models.RaceGetController;
 import models.RacePostController;
 
-
 @WebServlet("/race/*")
 public class RaceEventServlet extends HttpServlet {
-    @Inject
-    RacePostController racePostController;
-    @Inject
-    RaceGetController raceGetController;
+	@Inject
+	RacePostController racePostController;
+	@Inject
+	RaceGetController raceGetController;
 	private static final long serialVersionUID = 1L;
 	String regexCreatePattern = "/race/create";
 	String regexRacePattern = "(/race/)([0-9]+)(/[A-Za-z]*)?";
-	String regexRaceDetails = "/race/([0-9]+)/raceDetails";
+	String regexRaceDetails = "/race/([0-9]+)(/)?";
 	String regexRaceList = "/race/events";
 
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		String uriString = request.getRequestURI();
 		ModelAndView modelAndView = null;
-				
+
 		Pattern pattern = Pattern.compile(regexCreatePattern);
 		Matcher matcher = pattern.matcher(uriString);
-		
-		if(matcher.matches()){
+
+		if (matcher.matches()) {
 			modelAndView = raceGetController.createNewRaceEvent();
-			
+
 		}
-		
+
 		pattern = Pattern.compile(regexRaceList);
 		matcher = pattern.matcher(uriString);
-		
-		if(matcher.matches()){
-		
+
+		if (matcher.matches()) {
+
 			modelAndView = raceGetController.retrieveRaceEventList();
-			
+
 		}
-		
+
 		pattern = Pattern.compile(regexRacePattern);
 		matcher = pattern.matcher(uriString);
-		
-		if(matcher.find()){
-			if(matcher.group(3).equalsIgnoreCase("/update")){
-				modelAndView = raceGetController.updateRace(Long.parseLong(matcher.group(2)));
-			}
-			else if(matcher.group(3).equalsIgnoreCase("/delete")){
-				modelAndView = raceGetController.deleteRace(Long.parseLong(matcher.group(2)));
+
+		if (matcher.matches()) {
+			if (matcher.group(3) != null) {
+				if (matcher.group(3).equalsIgnoreCase("/update")) {
+					modelAndView = raceGetController.updateRace(Long
+							.parseLong(matcher.group(2)));
+				} else if (matcher.group(3).equalsIgnoreCase("/delete")) {
+					modelAndView = raceGetController.deleteRace(Long
+							.parseLong(matcher.group(2)));
+				}
 			}
 		}
-		
+
 		pattern = Pattern.compile(regexRaceDetails);
 		matcher = pattern.matcher(uriString);
-		
-		if(matcher.find()){
+
+		if (matcher.find()) {
 			Long id = Long.parseLong(matcher.group(1));
 			modelAndView = raceGetController.retrieveRace(id);
-			
+
 		}
-		
+
 		request.setAttribute("model", modelAndView.getModel());
-		RequestDispatcher view = request.getRequestDispatcher(modelAndView.getViewName());
+		RequestDispatcher view = request.getRequestDispatcher(modelAndView
+				.getViewName());
 		view.forward(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		String uriString = request.getRequestURI();
 		ModelAndView modelAndView = null;
 		Pattern pattern = Pattern.compile(regexCreatePattern);
 		Matcher matcher = pattern.matcher(uriString);
-		
-		if(matcher.matches()){
+
+		if (matcher.matches()) {
 			modelAndView = racePostController.createNewRaceEvent();
-            
+
 		}
-		
+
 		pattern = Pattern.compile(regexRacePattern);
 		matcher = pattern.matcher(uriString);
-		
-		if(matcher.matches()){
-			if(matcher.group(3).equalsIgnoreCase("/update")){
-			modelAndView = racePostController.updateRace(Long.parseLong(matcher.group(2)));
+
+		if (matcher.matches()) {
+			if (matcher.group(3).equalsIgnoreCase("/update")) {
+				modelAndView = racePostController.updateRace(Long
+						.parseLong(matcher.group(2)));
 			}
-			
+
 		}
-		
+
 		request.setAttribute("model", modelAndView.getModel());
-        RequestDispatcher view = request.getRequestDispatcher(modelAndView.getViewName());
-        view.forward(request, response);
+		RequestDispatcher view = request.getRequestDispatcher(modelAndView
+				.getViewName());
+		view.forward(request, response);
 	}
 }
