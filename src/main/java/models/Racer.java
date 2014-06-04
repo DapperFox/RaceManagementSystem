b978b7e9620f277473594d8f907b4432f51829d6
DapperFox <blakesterbishop@gmail.com>
2014-06-03 18:53:19 -0600

    Created a password field for our awesome hashed password!

--- src/main/java/models/Racer.java
+++ src/main/java/models/Racer.java
@@ -17,13 +17,16 @@ public class Racer {
 	
 	@Column(name="first_name")
 	private String firstName;
-	
+
 	@Column(name="last_name")
 	private String lastName;
 	
 	@Column(name="email")
 	private String email;
-	
+
+    @Column(name="password")
+    private String password;
+
 	@Column(name="gender")
 	private String gender;
 	
@@ -68,6 +71,14 @@ public class Racer {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 	public String getGender() {
 		return gender;
